a01cfecf6779be8751684172b544374df8d6a300
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-05-25 11:12:28 +0200

    init window, create mlx struct

--- includes/thomas.h
+++ includes/thomas.h
@@ -6,15 +6,30 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:54:07 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/05/24 10:36:00 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/05/25 11:04:52 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef THOMAS_H
 # define THOMAS_H
 # include "cub3d.h"
+# include "mlx_linux/mlx.h"
 
-void	display_map(t_map *map);
+# define ESC 65307
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define L_ARROW 65361
+# define R_ARROW 65363
+
+typedef struct s_mlx
+{
+	void	*ptr;
+	void	*win;
+}			t_mlx;
+
+void	init_mlx(t_mlx *mlx, t_map *map);
 void	init_raycasting(t_map *map);
 
 #endif
