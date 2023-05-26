2b31ee60c645337d4f54ced8e5674638be976f62
Modzie <modzielol@gmail.com>
2023-05-26 20:12:46 +0200

    add vector to .

--- includes/thomas.h
+++ includes/thomas.h
@@ -3,33 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   thomas.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msapin <msapin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:54:07 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/05/25 16:18:47 by msapin           ###   ########.fr       */
+/*   Updated: 2023/05/26 20:10:37 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef THOMAS_H
 # define THOMAS_H
 # include "cub3d.h"
-# include "mlx_linux/mlx.h"
+# include "math.h"
+# include "mlx/mlx.h"
 
-// # define ESC 65307
-// # define W 119
-// # define A 97
-// # define S 115
-// # define D 100
-// # define L_ARROW 65361
-// # define R_ARROW 65363
+// KEY MAC
+// #  define ESC 53
+// #  define W 13
+// #  define A 0
+// #  define S 1
+// #  define D 2
+// #  define M 46
+// #  define L_ARROW 123
+// #  define R_ARROW 124
 
-// typedef struct s_mlx
-// {
-// 	void	*ptr;
-// 	void	*win;
-// }			t_mlx;
+typedef struct s_vector2_f
+{
+	float	x;
+	float	y;
+}			t_vector2_f;
 
-// void	init_mlx(t_mlx *mlx, t_map *map);
-// void	init_raycasting(t_cub *cub);
+typedef struct s_vector2_d
+{
+	double	x;
+	double	y;
+}			t_vector2_d;
+
+typedef struct s_ray
+{
+	t_vector2_f	wall_coor;
+	t_vector2_d	wall_tiles;
+	double		length;
+	int			side;
+	double		angle;
+}				t_ray;
+
+void	where_am_i(t_cub *cub);
 
 #endif
--- sources/executing/raycasting.c
+++ sources/executing/raycasting.c
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mehdisapin <mehdisapin@student.42.fr>      +#+  +:+       +#+        */
+/*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:59:47 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/05/25 20:55:16 by mehdisapin       ###   ########.fr       */
+/*   Updated: 2023/05/26 19:32:52 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 #include "../../includes/thomas.h"
+
+int	check_keycode(int keycode, t_cub *cub);
+int	ft_exit(void);
 
 static void	display_map(t_map *map)
 {
@@ -19,17 +22,26 @@ static void	display_map(t_map *map)
 		printf("%s\n", map->array[i]);
 }
 
+// static void	raycaster(t_cub *cub)
+// {
+// 	cub->mlx = mlx_init();
+// 	cub->win = mlx_new_window(cub->mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+
+// 	mlx_hook(cub->win, 2, 1l << 0, check_keycode, cub);
+// 	mlx_hook(cub->win, 17, 1l << 0, ft_exit, cub);
+// 	mlx_loop(cub->mlx);
+// }
+
+void	where_am_i(t_cub *cub)
+{
+	printf("case : x = %d and y = %d\npixels : x = %d and y = %d\n", \
+	cub->p->pos_x, cub->p->pos_y, cub->p->mini_x, cub->p->mini_y);	
+}
+
 void	init_raycasting(t_cub *cub)
 {
 	(void)display_map;
-	// t_mlx	mlx;
-	// int		pos_x = 1;
-	// int		pos_y = 1;
-	// int		d_x = 1;
-	// int		d_y = 1;
-	// char		aim = 'N';
 
-	// display_map(cub->map);
+	// raycaster(cub);
 	init_mlx(cub);
-	// j'ai besoin de la position de depart du personnage ET orientation
 }
