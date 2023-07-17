98ad909538576c5f1bee7469565ec2863b79dd8f
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-07-17 11:48:13 +0200

    move_player now is bigger

--- sources/executing/movement.c
+++ sources/executing/movement.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 17:20:20 by mehdisapin        #+#    #+#             */
-/*   Updated: 2023/07/17 11:38:29 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/07/17 11:46:17 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,6 @@ void	rotate_player(t_cub *cub, int sign)
 	(cub->sin_angle * sign);
 }
 
-// void	move_player(t_cub *cub, t_vector axis, int sign)
-// {
-// 	t_vector	tmp_move;
-
-// 	tmp_move.x = (axis.x * SPEED_PLAYER) * sign;
-// 	tmp_move.y = (axis.y * SPEED_PLAYER) * sign;
-// 	cub->p->pos.x += tmp_move.x;
-// 	cub->p->pos.y += tmp_move.y;
-// 	if (cub->map->array[(int)(cub->p->pos.y)][(int)(cub->p->pos.x)] \
-// 	== '1')
-// 	{
-// 		cub->p->pos.x -= tmp_move.x;
-// 		cub->p->pos.y -= tmp_move.y;
-// 	}
-// }
-
 static int	is_blocked(double x, double y, char **map)
 {
 	if (map[(int)y][(int)x] == '1')
@@ -63,9 +47,10 @@ void	move_player(t_cub *cub, t_vector axis, int sign)
 
 	x = cub->p->pos.x + (axis.x * SPEED_PLAYER) * sign;
 	y = cub->p->pos.y + (axis.y * SPEED_PLAYER) * sign;
-	if (is_blocked(x, y, cub->map->array) || is_blocked(x + 0.1, y - 0.1, cub->map->array) \
-	|| is_blocked(x + 0.1, y + 0.1, cub->map->array) || is_blocked(x - 0.1, y + 0.1, cub->map->array) \
-	|| is_blocked(x - 0.1, y - 0.1, cub->map->array))
+	if (is_blocked(x, y, cub->map->array) || is_blocked(x + 0.1, y - 0.1, \
+	cub->map->array) || is_blocked(x + 0.1, y + 0.1, cub->map->array) || \
+	is_blocked(x - 0.1, y + 0.1, cub->map->array) || is_blocked(x - 0.1, \
+	y - 0.1, cub->map->array))
 		return ;
 	else
 	{
