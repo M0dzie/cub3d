3bd6755ef77fd268824e3b988ca3f9d819670304
Modzie <modzielol@gmail.com>
2023-07-26 22:42:05 +0200

    JE SUIS UNE LEGENDE (fix revert)

--- sources/executing/render_3d.c
+++ sources/executing/render_3d.c
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:41:02 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/07/26 22:26:32 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/07/26 22:40:26 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,28 +51,39 @@ static int	get_texture_pixel(t_xpm tex, int x, int y)
 	return (*(unsigned int *)dst);
 }
 
-static void	draw_wall(t_cub *cub, int x, int y, int max, t_ray_map *ray, int px_column)
+static void	draw_wall(t_cub *cub, int x, int y, int max, t_ray_map *ray)
 {
 	t_xpm	tex;
-	(void)px_column;
+	int tex_x;
+	int tex_y;
+	int tmp_y;
 
 	if (ray->side == EAST)
+	{
 		tex = cub->east;
-	else if (ray->side == WEST)
-		tex = cub->west;
-	else if (ray->side == SOUTH)
-		tex = cub->south;
-	else if (ray->side == NORTH)
-		tex = cub->north;
-	int tex_x = 0;
-	if (ray->side == NORTH || ray->side == SOUTH) // South and west need to be invert
-		tex_x = (cub->p->ray[x]->map.x - (int)cub->p->ray[x]->map.x) * tex.width;
-	else
 		tex_x = (cub->p->ray[x]->map.y - (int)cub->p->ray[x]->map.y) * tex.width;
-	int tex_y = 0;
-	int tmp_y = 0;
+	}
+	else if (ray->side == WEST)
+	{
+		tex = cub->west;
+		tex_x = (cub->p->ray[x]->map.y - (int)cub->p->ray[x]->map.y) * tex.width;
+		tex_x = tex.width - tex_x;
+	}
+	else if (ray->side == SOUTH)
+	{
+		tex = cub->south;
+		tex_x = (cub->p->ray[x]->map.x - (int)cub->p->ray[x]->map.x) * tex.width;
+		tex_x = tex.width - tex_x;
+	}
+	else
+	{
+		tex = cub->north;
+		tex_x = (cub->p->ray[x]->map.x - (int)cub->p->ray[x]->map.x) * tex.width;
+	}		
 	if (cub->p->ray[x]->wall_height > WIN_HEIGHT)
 		tmp_y = fabs((max / 2) - (cub->p->ray[x]->wall_height / 2));
+	else
+		tmp_y = 0;
 	while (y < max)
 	{
 		tex_y = tmp_y * (tex.height / cub->p->ray[x]->wall_height);
@@ -81,30 +92,6 @@ static void	draw_wall(t_cub *cub, int x, int y, int max, t_ray_map *ray, int px_
 		tmp_y++;
 		y++;
 	}
-	
-	// printf("tex_x = %d\n", tex_x);
-
-	// double coef_i;
-	// (void)coef_i;
-	// i = 0;
-
-	// if ((int)ray->wall_height < tex.height)
-	// 	coef_i = tex.height / ray->wall_height;
-	// else
-	// 	coef_i = 1;
-
-	// while (y < max)
-	// {
-	// 	if (i >= tex.height)
-	// 		i = 127;
-	// 	int color = tex.px[tex.height * i + px_column];
-	// 	// int color = 0x000362fc;
-	// 	(void)tex;
-	// 	put_pixel(&cub->imgs->back, x, y, color);
-	// 	if ((y - cub->p->ray[x]->margin) > (cub->p->ray[x]->wall_height / (double)cub->north.height * i))
-	// 		i++;
-	// 	y++;
-	// }
 }
 
 void	calcul_ray(t_cub *cub)
@@ -364,7 +351,7 @@ void	generate_3d(t_cub *cub)
 		// 	px_column = 128 - ((128 * ((cub->p->wall[cub->p->ray[x]->index_wall].percent_start) / 100)) + (tmp_index * percent_line));
 		// else
 		// 	px_column = (128 * ((cub->p->wall[cub->p->ray[x]->index_wall].percent_start) / 100)) + (tmp_index * percent_line);
-		int	px_column = 128;
+		// int	px_column = 128;
 
 
 		// if (px_column <= 0)
@@ -376,9 +363,9 @@ void	generate_3d(t_cub *cub)
 		// 	printf("px_column: %d\n", px_column);
 
 		if (cub->p->ray[x]->margin > 0 && cub->p->ray[x]->margin < WIN_HEIGHT)
-			draw_wall(cub, x, cub->p->ray[x]->margin, cub->p->ray[x]->margin + cub->p->ray[x]->wall_height - 1, cub->p->ray[x], px_column);  // temporary
+			draw_wall(cub, x, cub->p->ray[x]->margin, cub->p->ray[x]->margin + cub->p->ray[x]->wall_height - 1, cub->p->ray[x]);  // temporary
 		else
-			draw_wall(cub, x, 0, WIN_HEIGHT, cub->p->ray[x], px_column);  // temporary
+			draw_wall(cub, x, 0, WIN_HEIGHT, cub->p->ray[x]);  // temporary
 		// tmp_index++;
 		// // printf("wall %d/   side: %d   width: %d   percent_start: %f   percent_end: %f\n", x, cub->p->ray[x]->side, cub->p->wall[x].width, cub->p->wall[x].percent_start, cub->p->wall[x].percent_end);
 	}
